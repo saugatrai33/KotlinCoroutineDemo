a3efef1e6452dccebd6c75b7ad0043ed037ffc63
Saugat Rai <saugatrai33@gmail.com>
2021-09-25 17:02:49 +0545

    coroutine jobs

--- app/src/main/java/com/example/kotlincoroutinedemo/MainActivity.kt
+++ app/src/main/java/com/example/kotlincoroutinedemo/MainActivity.kt
@@ -1,79 +1,96 @@
 package com.example.kotlincoroutinedemo
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.TextView
+import android.widget.Toast
 import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
 
 class MainActivity : AppCompatActivity() {
 
-    private val RESULT_1: String = "Result #1"
-    private val RESULT_2: String = "Result #2"
-    private val TIMEOUT: Long = 1900L
+    private val PROGRESS_MAX = 100
+    private val PROGRESS_START = 0
+    private val JOB_TIME = 400 // ms
 
+    private lateinit var progressBar: ProgressBar
+    private lateinit var btnStartJob: Button
     private lateinit var textHelloWorld: TextView
 
+    private lateinit var job: CompletableJob
+
+    @DelicateCoroutinesApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnClickMe: Button = findViewById(R.id.btnClickMe)
+        progressBar = findViewById(R.id.progressBar)
+        btnStartJob = findViewById(R.id.btnStartJob)
         textHelloWorld = findViewById(R.id.textHelloWorld)
 
-        btnClickMe.setOnClickListener {
-            CoroutineScope(IO).launch {
-                fakeAPIRequest()
+        btnStartJob.setOnClickListener {
+            if (!::job.isInitialized) initJob()
+            progressBar.startOrCancelJob(job)
+        }
+    }
+
+    @DelicateCoroutinesApi
+    private fun initJob() {
+        updateJobCompleteTextView("")
+        job = Job()
+        job.invokeOnCompletion {
+            it?.message.let {
+                var msg = it
+                if (msg.isNullOrEmpty()) {
+                    msg = "Unknown Cancellation error."
+                }
+                println("$job was cancelled. reason $msg")
+                showToast(this, msg)
+            }
+        }
+        progressBar.max = PROGRESS_MAX
+        progressBar.progress = PROGRESS_START
+    }
+
+    private fun ProgressBar.startOrCancelJob(job: Job) {
+        if (this.progress > 0) {
+            println("$job is already active. cancelling.....")
+            resetJob()
+        } else {
+            btnStartJob.text = "Cancel Job"
+            CoroutineScope(IO + job).launch {
+                println("Coroutine $this is activated with $job")
+                for (i in PROGRESS_START..PROGRESS_MAX) {
+                    delay((JOB_TIME / PROGRESS_MAX).toLong())
+                    this@startOrCancelJob.progress = i
+                }
+                updateJobCompleteTextView("Job Completed.")
             }
         }
     }
 
-    private fun setHelloWorldText(input: String) {
-        val currentText = textHelloWorld.text.toString()
-        textHelloWorld.text = currentText + "\n$input"
+    private fun resetJob() {
+        if (job.isActive || job.isCompleted) {
+            job.cancel(CancellationException("Resetting Job"))
+        }
+        initJob()
     }
 
-    private suspend fun setTextOnMainThread(text: String) {
-        withContext(Main) {
-            setHelloWorldText(text)
+    private fun updateJobCompleteTextView(input: String) {
+        GlobalScope.launch(Main) {
+            textHelloWorld.setText(input)
         }
     }
 
-    private suspend fun fakeAPIRequest() {
-        val job = withTimeoutOrNull(TIMEOUT){
-            val result1 = getResultFromAPI1()
-            println("debug: $result1")
-            setTextOnMainThread(result1)
+}
 
-            val result2 = getResultFromAPI2()
-            println("debug: $result2")
-            setTextOnMainThread(result2)
-        }
-
-        if (job == null) {
-            val cancelMessage = "Cancelling job.... job tooks longer than $TIMEOUT"
-            println("debug: $cancelMessage")
-            setTextOnMainThread(cancelMessage)
-        }
-
+@DelicateCoroutinesApi
+fun showToast(context: Context, msg: String) {
+    GlobalScope.launch(Main) {
+        Toast.makeText(context, msg, Toast.LENGTH_LONG).show()
     }
-
-    private suspend fun getResultFromAPI2(): String {
-        logThread("getResultFromAPI2")
-        delay(1000)
-        return RESULT_2
-    }
-
-    private suspend fun getResultFromAPI1(): String {
-        logThread("getResultFromAPI1")
-        delay(1000)
-        return RESULT_1
-    }
-
-    private fun logThread(methodName: String) {
-        println("debug: $methodName: ${Thread.currentThread().name}")
-    }
-
 }
